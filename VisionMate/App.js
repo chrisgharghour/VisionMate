13c6900f61702c5fe4c55c15a9bd469472e42785
Missy Bridgwater <missy789@g.ucla.edu>
2024-01-13 10:34:44 -0800

    Added basic buttons

--- VisionMate/App.js
+++ VisionMate/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Button from './components/Button';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button theme="primary" label="Button 1"></Button>
+      <Button label="Button 2"></Button>
       <StatusBar style="auto" />
     </View>
   );
---
+++ VisionMate/Components/Button.js
@@ -0,0 +1,58 @@
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+// import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+// Using inline styles allows overriding the default styles for a specific value
+
+export default function Button({ label, theme }) {
+    if (theme === "primary") {
+        return (
+          <View
+          style={[styles.buttonContainer, { borderColor: "#ffd33d" }]}
+          >
+            <Pressable
+              style={[styles.button, { backgroundColor: "#fff" }]}
+              onPress={() => alert('You pressed a button.')}
+            >
+              <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+            </Pressable>
+        </View>
+        );
+    }
+    return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={() => alert('You pressed a button made by missy.')}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+    borderColor: "#000",
+    borderWidth: 4,
+    borderRadius: 18
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  // not actively using buttonIcon as we dont have one right now but optional to add
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: '#000',
+    fontSize: 16,
+  },
+});
